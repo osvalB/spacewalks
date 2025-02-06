c9fb7fb57ca825c6f15ec53ff124164af3157ee6
osvalB <oburastero@gmail.com>
2025-02-06 12:02:54 +0100

    add crew size fx and test

--- test_code.py
+++ test_code.py
@@ -1,8 +1,29 @@
+import pytest
 from eva_data_analysis import text_to_duration
+from eva_data_analysis import calculate_crew_size
+
+def test_text_to_duration_float():
+
+    """
+    Test that text_to_duration returns expected float value
+    for durations with a non-zero minute component
+    """
+
+    assert abs(text_to_duration('10:20') - 10.3333333) < 1e-5
 
 def test_text_to_duration_integer():
-    input_value = '10:00'
-    test_results = text_to_duration(input_value) == 10
-    print(f'text_to_duration({input_value}) == 10? {test_results}')
 
-test_text_to_duration_integer()
+    """
+    Test that text_to_duration returns expected integer value
+    for durations with a zero minute component
+    """
+
+    assert text_to_duration('10:00') == 10
+
+def test_calculate_crew_size():
+    """
+    Test that calculate_crew_size returns the expected crew size
+    """
+    actual_result = calculate_crew_size('sara doe; john connor;')
+    expected_result = 2
+    assert actual_result == expected_result
